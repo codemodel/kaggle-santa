cdeafa9c52fca14860ab5bc68bb0db0031d9633d
JN <janne.nevalainen@iki.fi>
2017-06-02 12:15:10 +0300

    reorganized code

--- simple analysis.R
+++ simple analysis.R
@@ -1,29 +1,16 @@
 #data = read.csv('gifts.csv')
 
-#pick each gift on separate sleigh
+#initialize
+install.packages("geosphere")
 
+#
 library(geosphere)
-NorthPole = c(0,90)
-SleightWeight =10
-
-solution = data.frame(matrix(0, ncol = 2, nrow = 100000))  
-colnames(solution) <- c("GiftId","TripId")
-
-  
-solution$GiftId = data$GiftId
-solution$TripId = data$GiftId
-
-write.csv(solution, file = "solution_simple.csv", row.names=FALSE)
-
-# score from kaggle: 29121011015.58950
-# # ie 29 
-#leader was 123959.. billion
+data = read.csv('gifts.csv') # location of gifts on globe: latitude, longitude
+NorthPole = c(0,90) # sleights start and return with cargo to here
+SleightWeight =10 # weight of sleigh adds to score (to be minimized)
 
 
-# theoretical lower limit:  1.205621e+13 (not indlucing sleigh weight)
-12 042 715 199
-#calculate score
-
+#pick each gift on separate sleigh, getting initial higher limit
 total=0
 for (i in 1:nrow(data)) {
   x = data$Longitude[i]
@@ -32,12 +19,15 @@ for (i in 1:nrow(data)) {
   #dist = distHaversine(NorthPole, p1, r=6378137)  , meters, need km
   dist = distHaversine(NorthPole, p1, r=6371)  
   total = total +( dist*data$Weight[i])+ 2*dist*SleightWeight
-  }
+}
+
+# score from kaggle: 29121011015.58950
+# ie 29 billion
+# leader was 12.3959 billion
+
 
 
 #theoretical lower limit, each packets weight on direct route from NorthPole, cant go under this
-# 12 042 715 199
-
 total=0
 for (i in 1:nrow(data)) {
   x = data$Longitude[i]
@@ -46,3 +36,14 @@ for (i in 1:nrow(data)) {
   dist = distHaversine(NorthPole, p1, r=6371)  
   total = total +( dist*data$Weight[i])
 }
+
+# theoretical lower limit:
+#12 042 715 199
+
+
+# format simple solution and write it to csv
+solution = data.frame(matrix(0, ncol = 2, nrow = 100000))  
+colnames(solution) <- c("GiftId","TripId")
+solution$GiftId = data$GiftId
+solution$TripId = data$GiftId
+write.csv(solution, file = "solution_simple.csv", row.names=FALSE)
